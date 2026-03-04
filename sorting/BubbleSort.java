da6b5aee1b1e5f5ca3b14cb8941f772c0b00bf0b
Surender K M <surenderkm11a2svv@gmail.com>
2026-03-04 20:23:58 +0530

    Updated TC

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -17,14 +17,18 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr, int n) {
        for(int i=0;i<n;i++)
        {
+         int didSwap = 0;
          for(int j=0;j<n-i-1;j++)
          {
             if(arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                didSwap = 1;
             }
          }
+         if(didSwap==0) return; //O(N) - Best case
+         System.out.println("Given not in sorted order");
        }
     }
 
